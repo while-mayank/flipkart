82ef42e9855d8469c740a4d13b0d572b88a84885
Mayank Sinha <mayank.sinha@oaktreecloud.com>
2022-10-31 15:42:42 +0530

    debugged root url.

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,4 @@ ActiveRecord::Schema.define(version: 2022_09_27_124837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
